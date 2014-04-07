2c09d86ce0e5b79d3eee6c41e7b541b83597f228
Dwalu Z. Khasu <dwaluk@gmail.com>
2014-04-06 23:20:30 -0700

    More corrections come

--- language/en/view_albums.php
+++ language/en/view_albums.php
@@ -3,9 +3,9 @@
 $tv = array(
     'g_addalbum'=>'Add album',
                                   'g_title'=>'Name of album',
-                                  'g_access'=>'Album grant',
+                                  'g_access'=>'Grant permission',
                                   'g_add'=>'Add',
-                                  'g_renamealbum'=>'Rename this album',
+                                  'g_renamealbum'=>'Rename album',
                                       
     
     
