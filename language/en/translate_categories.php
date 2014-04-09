f5443797178e83b7549ab5a9549ed44446134336
Dwalu Z. Khasu <dwaluk@gmail.com>
2014-04-09 03:25:42 -0700

    More english changes for contextual review

--- language/en/translate_categories.php
+++ language/en/translate_categories.php
@@ -2,8 +2,8 @@
 
 $h = array(
     'invalid_address'=>'Invalid address',
-                                  'cat_added'=>'Categorie added',
-                                  'ct_correctinfo'=>'Please correct your informations !!',
+                                  'cat_added'=>'Category added',
+                                  'ct_correctinfo'=>'Please correct your information',
                                   'tag_added'=>'Tag added',
                                   'still_have_tag'=>'Still have this tag',
                                       
